a2d1b4367c0d660a24f876dced6c8ffd431dcc5d
YoloFmaster <yolofmaste@gmail.com>
2023-11-17 16:40:18 +0300

    Нормальная программа

--- src/main/kotlin/Class.kt
+++ src/main/kotlin/Class.kt
@@ -1,16 +1,15 @@
 class CashiersOffice(minSoldTicekts: Int, maxSoldTickets: Int) {
 
-    val a = minSoldTicekts
-    val b = maxSoldTickets
+    private val a = minSoldTicekts
+    private val b = maxSoldTickets
     fun soldTickets(): Int {
-        val soldTikets = (a .. b).random()
-        return soldTikets
+        return (a..b).random()
     }
 }
 
 class Itinerary {
 
-    val city = arrayListOf(
+    private val city = arrayListOf(
         "Москва","Санкт-Петербург","Уфа",
         "Омск","Томск","Тамбов",
         "Стерлитомак","Орёл","Екатиренбург",
@@ -25,21 +24,20 @@ class Itinerary {
         if (city[primaryCity] != city[endCity]){
             route.add(city[primaryCity])
             route.add(city[endCity])
-            return route
         }
         else {
             endCity = (0 .. 14).random()
             route.add(city[primaryCity])
             route.add(city[endCity])
-            return route
         }
+        return route
     }
 }
 
 class Train {
     fun creatingRailcars(passenger: Int): ArrayList<Int> {
         var capacity: Int
-        var countRailcars = ArrayList<Int>()
+        val countRailcars = ArrayList<Int>()
         var capacityRailcars = 0
         do {
             capacity = (5 .. 25).random()
